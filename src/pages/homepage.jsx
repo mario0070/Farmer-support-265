e815c9643c52d81b3a94d87d67174517f11f26ef
muhammadjamiu abdulganiyu <ganiujamiu03@gmail.com>
2023-11-21 17:15:23 +0100

    Brower Router

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 
 export default function Homepage() {
   return (
-    <div>Homepage</div>
+    <>
+      <div>Homepage</div>
+      <a href="/login">Login</a>
+    </>
   )
 }

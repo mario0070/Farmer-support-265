2acfbb1d4c3c779a212de9befa9539f6860d7302
muhammadjamiu abdulganiyu <ganiujamiu03@gmail.com>
2023-11-26 03:10:37 +0100

    AI messages

--- src/pages/chat-bot.jsx
+++ src/pages/chat-bot.jsx
@@ -46,115 +46,115 @@ export default function ChatBot() {
     });
   }
 
-  var data = JSON.parse(Cookies.get('user_token'))
-  const user_data = JSON.parse(Cookies.get('user'))
-  const str = user_data.farmerName
-  const farmerName = str.substring(
-    str.indexOf(" ") + 1, 
-    str.lastIndexOf(";")
-  );
-
-  let send = axios.create({
-    baseURL: 'https://farmer-support-api.onrender.com/',
-    headers: {
-        "Authorization" : `Bearer ${data.token}`,
-        'Access-Control-Allow-Origin' : '*',
-        'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE,PATCH,OPTIONS',
-    },
-  });
-
-  const postMsg = (id, msg) => {
-    setAIresponse(true)
-    const data =  {
-      "role": "user", 
-      "content": msg 
-    }
-    $(".msg-container").scrollTop($(".msg-container").height()*200);
-    send.post("/assistant",{
-      "chatId": msgId, 
-      "title": "", 
-      "message": recieveMsg.concat(data),
-    }).then(res => {
-      setmsgId(res.data.conversation._id)
-      setrecieveMsg(res.data.conversation.chats)
-      setAIresponse(false)
-      console.log(res)
-      AImsg(res.data.conversation.chats[res.data.conversation.chats.length - 1].content)
-    })
-    .catch(err => {
-      console.log(err)
-    })
-  }
-
-  const AImsg = (msg) => {
-    $(".msg-container").append(`
-      <div class="wrap1 unique">
-      <div class="">
-          <p class='mb-0 mx-3'><i class="fa-brands fa-bots"></i></p>
-          <div class="msgBodys mt-0">
-              <p class='mb-0 p-2'>${msg}</p>
-          </div>
-      </div>
-    </div>`)
-    $(".msg-container").scrollTop($(".msg-container").height()*200);
-  }
-
-  const sendMsg = () => {
-    if(msgInputs.current.value != ""){
-      $(".msg-container").append(`
-      <div class="wrap2 unique mt -2">
-      <p class='mb-0 msgIcon mx-3 text-end mb-0'>
-      <img src=${farmer} alt="" className='' width=${20} />
-      </p>
-      <div class="sentMsg mt-0">
-          <div class="myMsg">
-              <p class="mb-0 p-2">${msgInputs.current.value}</p>
-          </div>
-        </div>
-      </div>`)
-      $(".msg-container").scrollTop($(".msg-container").height()*200);
-      postMsg("",msgInputs.current.value)
-      msgInputs.current.value = ""
-    }else{
-      alert("warning","Please type a message")
-    }
-  }
-
-  const msgInput = (e) => {
-    setMSg(e.target.value)
-  }
-
-  const getConversation = (index) => {
-    setConverse(true)
-    send.get("/assistant/history",{
-    }).then(res => {
-      $(".unique").hide()
-      setConverse(false)
-      setConversation(res.data.conversationHistory[index].chats)
-      // console.log(res.data.conversationHistory[index].chats,index)
-    })
-    .catch(err => {
-      console.log(err)
-    })
-  }
-
-  useEffect(() => {
-    send.get("/assistant/history",{
-    }).then(res => {
-      // console.log(res.data.conversationHistory)
-      setHistory(res.data.conversationHistory)
-      setisloaded(true)
-    })
-    .catch(err => {
-      console.log(err)
-    })
-    setUsername(farmerName)
-  },[history])
-
   if(!cookie.user_token){
     window.location.href = "/login"
   }
   else{
+      var data = JSON.parse(Cookies.get('user_token'))
+      const user_data = JSON.parse(Cookies.get('user'))
+      const str = user_data.farmerName
+      const farmerName = str.substring(
+        str.indexOf(" ") + 1, 
+        str.lastIndexOf(";")
+      );
+  
+      let send = axios.create({
+        baseURL: 'https://farmer-support-api.onrender.com/',
+        headers: {
+            "Authorization" : `Bearer ${data.token}`,
+            'Access-Control-Allow-Origin' : '*',
+            'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE,PATCH,OPTIONS',
+        },
+      });
+
+      const postMsg = (id, msg) => {
+        setAIresponse(true)
+        const data =  {
+          "role": "user", 
+          "content": msg 
+        }
+        $(".msg-container").scrollTop($(".msg-container").height()*200);
+        send.post("/assistant",{
+          "chatId": msgId, 
+          "title": "", 
+          "message": recieveMsg.concat(data),
+        }).then(res => {
+          setmsgId(res.data.conversation._id)
+          setrecieveMsg(res.data.conversation.chats)
+          setAIresponse(false)
+          console.log(res)
+          AImsg(res.data.conversation.chats[res.data.conversation.chats.length - 1].content)
+        })
+        .catch(err => {
+          console.log(err)
+        })
+      }
+    
+      const AImsg = (msg) => {
+        $(".msg-container").append(`
+          <div class="wrap1 unique">
+          <div class="">
+              <p class='mb-0 mx-3'><i class="fa-brands fa-bots"></i></p>
+              <div class="msgBodys mt-0">
+                  <p class='mb-0 p-2'>${msg}</p>
+              </div>
+          </div>
+        </div>`)
+        $(".msg-container").scrollTop($(".msg-container").height()*200);
+      }
+    
+      const sendMsg = () => {
+        if(msgInputs.current.value != ""){
+          $(".msg-container").append(`
+          <div class="wrap2 unique mt -2">
+          <p class='mb-0 msgIcon mx-3 text-end mb-0'>
+          <img src=${farmer} alt="" className='' width=${20} />
+          </p>
+          <div class="sentMsg mt-0">
+              <div class="myMsg">
+                  <p class="mb-0 p-2">${msgInputs.current.value}</p>
+              </div>
+            </div>
+          </div>`)
+          $(".msg-container").scrollTop($(".msg-container").height()*200);
+          postMsg("",msgInputs.current.value)
+          msgInputs.current.value = ""
+        }else{
+          alert("warning","Please type a message")
+        }
+      }
+    
+      const msgInput = (e) => {
+        setMSg(e.target.value)
+      }
+    
+      const getConversation = (index) => {
+        setConverse(true)
+        send.get("/assistant/history",{
+        }).then(res => {
+          $(".unique").hide()
+          setConverse(false)
+          setConversation(res.data.conversationHistory[index].chats)
+          // console.log(res.data.conversationHistory[index].chats,index)
+        })
+        .catch(err => {
+          console.log(err)
+        })
+      }
+
+      useEffect(() => {
+        send.get("/assistant/history",{
+        }).then(res => {
+          // console.log(res.data.conversationHistory)
+          setHistory(res.data.conversationHistory)
+          setisloaded(true)
+        })
+        .catch(err => {
+          console.log(err)
+        })
+        setUsername(farmerName)
+      },[history])
+
     return (
       <div className='dashboard'>
       <CustomSidebar/>
